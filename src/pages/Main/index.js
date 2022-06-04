7bd092b2e4ab2c51560d70abaaecfb3e7f0dd4af
Artūras_M <arturas.miks@GMAIL.COM>
2022-06-04 18:03:30 +0300

    fixed games page mobile response, hided side menu

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import SideMenu from "../../components/SideMenu";
 import Games from "../../components/Games";
 import {Switch, Route} from 'react-router-dom';
+import {FaFilter} from "react-icons/fa";
 
 const Main = () => {
+    const [open, setOpen] = useState(false);
 
     return (
         <main className='main'>
-            <div className="main__menu">
+            <div className={`main__menu ${open ? 'open' : ''}`}>
                 <SideMenu/>
+                <div onClick={() => setOpen(!open)} className="main__menu-mobile-icon-box">
+                    <FaFilter className="main__menu-mobile-icon"/>
+                </div>
             </div>
             <div className="main__games">
                 <Games/>
